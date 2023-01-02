bb1036025efe13e6e5048100b2a3e57c9b644dd5
samir93bj <samir93bj@gmail.com>
2023-01-02 20:45:23 -0300

    [Feat: Jetstream]: Config reautilize jetstream

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,4 +1,19 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+@php
+    $nav_links = [
+        [
+            'name'     => 'Dashboard',
+            'route'    => route('dashboard'),
+            'active'   => request()->routeIs('dashboard')
+        ],
+        [
+            'name'     => 'Prueba',
+            'route'    => route('dashboard'),
+            'active'   => false
+        ]
+    ]
+@endphp
+
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 shadow">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -12,9 +27,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-jet-nav-link>
+                    @foreach ($nav_links as $nav_link)
+                        <x-jet-responsive-nav-link href="{{ $nav_link['route'] }}" :active="$nav_link['active']">
+                            {{ $nav_link['name'] }}
+                        </x-jet-responsive-nav-link>
+                    @endforeach
                 </div>
             </div>
 
@@ -137,9 +154,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-jet-responsive-nav-link>
+            @foreach ($nav_links as $nav_link)
+                <x-jet-responsive-nav-link href="{{ $nav_link['route'] }}" :active="$nav_link['active']">
+                    {{ $nav_link['name'] }}
+                </x-jet-responsive-nav-link>
+            @endforeach
         </div>
 
         <!-- Responsive Settings Options -->
