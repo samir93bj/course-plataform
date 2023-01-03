9d9c77d4581e987621f43300479f2c320f2d8c0a
samir93bj <samir93bj@gmail.com>
2023-01-02 21:21:19 -0300

    [Feat view]: Config View Hoome

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,12 +15,12 @@
 
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 shadow">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto mt-3 px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-jet-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
@@ -28,9 +28,9 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     @foreach ($nav_links as $nav_link)
-                        <x-jet-responsive-nav-link href="{{ $nav_link['route'] }}" :active="$nav_link['active']">
+                        <x-jet-nav-link href="{{ $nav_link['route'] }}" :active="$nav_link['active']">
                             {{ $nav_link['name'] }}
-                        </x-jet-responsive-nav-link>
+                        </x-jet-nav-link>
                     @endforeach
                 </div>
             </div>
@@ -88,7 +88,8 @@
 
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
-                    <x-jet-dropdown align="right" width="48">
+                    @auth
+                        <x-jet-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                 <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
@@ -135,7 +136,11 @@
                                 </x-jet-dropdown-link>
                             </form>
                         </x-slot>
-                    </x-jet-dropdown>
+                        </x-jet-dropdown>
+                    @else
+                        <a href="{{ route('login')}}"    class="text-sm text-gray-700 underline">Login</a>
+                        <a href="{{ route('register')}}" class="text-sm text-gray-700 underline">Register</a>
+                    @endauth
                 </div>
             </div>
 
@@ -162,7 +167,8 @@
         </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        @auth
+            <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="shrink-0 mr-3">
@@ -229,6 +235,16 @@
                     @endforeach
                 @endif
             </div>
-        </div>
+            </div>
+        @else
+            <div class="py-1 border-t border-gray-200">
+                <x-jet-responsive-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+                    Login
+                </x-jet-responsive-nav-link>
+                <x-jet-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+                    Register
+                </x-jet-responsive-nav-link>
+            </div>
+        @endauth
     </div>
 </nav>
